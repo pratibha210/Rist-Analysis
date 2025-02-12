03969ac79da67594026f094c2d0c11aed54c0243
pratibha210 <pratibha.thakur@onenesstechs.in>
2025-02-12 18:57:36 +0530

    changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig(({ command }) => {
-//   const isPrroduction = command === 'build';
-//   return {
-//     plugins: [react()],
-//     base: isPrroduction ? "/src/main.jsx" : "/"
-//   };
-// });
